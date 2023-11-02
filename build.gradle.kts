3c06a13a4a0a11b83b8536560fda4a00f8c154c0
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-11-02 11:39:45 -0700

    Add license

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import net.kyori.indra.IndraExtension
+import net.kyori.indra.licenser.spotless.IndraSpotlessLicenserExtension
 
 plugins {
     alias(libs.plugins.indra) apply false
+    alias(libs.plugins.indraLicenser) apply false
     alias(libs.plugins.indraSonatypePublishing)
 }
 
@@ -11,8 +13,9 @@ indraSonatype {
 
 subprojects {
     plugins.apply("net.kyori.indra")
+    plugins.apply("net.kyori.indra.licenser.spotless")
 
-    the<IndraExtension>().apply {
+    extensions.getByType<IndraExtension>().apply {
         configurePublications {
             pom {
                 developers {
@@ -27,5 +30,11 @@ subprojects {
         github("jpenilla", "gremlin") {
             ci(true)
         }
+
+        apache2License()
+    }
+
+    extensions.getByType<IndraSpotlessLicenserExtension>().apply {
+        licenseHeaderFile(rootProject.file("LICENSE_HEADER"))
     }
 }
